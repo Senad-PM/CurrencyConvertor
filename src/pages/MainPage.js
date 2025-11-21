d637dd3aaf8d6f11bdc5475d3ff3722954e32ba9
Prashasthi <malithsenad@gmail.com>
2025-11-21 11:23:23 +0530

    Add files via upload

---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MainPage() {
+  return (
+    <div>
+        <h1>Convert Your Currencies Today</h1>
+        <p>Convert Your Currencies Today is a fast, reliable, and user-friendly 
+            currency conversion platform designed to help you exchange currencies 
+            with ease. Whether you're traveling abroad, shopping internationally, 
+            making business transactions, or simply tracking exchange rates, our tool 
+            provides real-time currency conversions for over 150 world currencies.</p>
+    </div>
+  )
+}
